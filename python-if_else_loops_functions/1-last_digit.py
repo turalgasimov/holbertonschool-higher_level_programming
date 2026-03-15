0a6e1b5bf5bdd39ffa64802714913c843251b2df
turalgasimov <turallgasimov@gmail.com>
2026-03-15 21:36:26 +0000

    1-last_digit.py

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+
+lastDigit = number % 10
+if number < 0:
+    lastDigit += -10
+str = (
+    "and is greate than 5" 
+    if lastDigit > 5 
+    else "and is less than 6 and not 0" 
+    if lastDigit < 6 and lastDigit != 0 else "and is 0")
+
+print(f'Last digit of {number} is {lastDigit} {str}')
--- python-if_else_loops_functions/README.md
+++ python-if_else_loops_functions/README.md
@@ -1,4 +1,6 @@
 List of the files and their purposes:
 
-0-positive_or_negative.py ->  assign a random signed number to the variable.
+0-positive_or_negative.py -> assign a random signed number to the variable.
+
+1-last_digit.py -> assign a random signed number to the variable.
 
