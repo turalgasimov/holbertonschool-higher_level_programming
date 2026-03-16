9d673dda9bdcc39beb49edd4f2e3b6ddf77444ec
turalgasimov <turallgasimov@gmail.com>
2026-03-16 22:53:50 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
