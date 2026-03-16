9c55ca0105eecd4e8b6b678787f7c68fad11eff8
turalgasimov <turallgasimov@gmail.com>
2026-03-16 03:00:34 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for i in range(101):
-        s = ', '
-        if i == 0:
-            print(0, end=s)
+    for i in range(1, 101):
+        s = ' '
         if i == 100:
             s = '\n'
         if i % 3 == 0:
