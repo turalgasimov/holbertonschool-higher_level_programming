6cc78cb8b35b33aa24d5fec2f8347133575c5b53
turalgasimov <turallgasimov@gmail.com>
2026-03-16 01:34:12 +0000

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,12 +4,10 @@ def uppercase(str):
     s = ''
     size = len(str)
     for i in range(len(str)):
-        if i == size - 1:
+        if i == size - 1 or size == 0:
             s = '\n'
         if ord('a') <= ord(str[i]) <= ord('z'):
             print('{}'.format(chr(ord(str[i]) - 32)), end=s)
         else:
-            print(str[i], end='')
+            print(str[i], end=s)
             continue
-
-uppercase("Best School 98 Battery streeT")
