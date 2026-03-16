3f6cb43108123da31df3835a8b562edf6c63967e
turalgasimov <turallgasimov@gmail.com>
2026-03-16 06:24:00 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,6 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
-        return
     elif len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
