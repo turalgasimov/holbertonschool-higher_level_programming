2df1e35d711cec2a5968ed88a859977c5114ce09
turalgasimov <turallgasimov@gmail.com>
2026-03-16 22:50:07 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-
-from calculator_1 import add, sub, mul, div, sys
+import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == "+":
+        res = add(a, b)
+    elif op == "-":
+        res = sub(a, b)
+    elif op == "*":
+        res = mul(a, b)
+    elif op == "/":
+        res = div(a, b)
     else:
-        op = sys.argv[2]
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        res = 0
-        match op:
-            case "+":
-                res = add(a, b)
-            case "-":
-                res = sub(a, b)
-            case "*":
-                res = mul(a, b)
-            case "/":
-                res = div(a, b)
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
-        print('{} {} {} = {}'.format(a, op, b, res))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, res))
