f07c8b0ccdbc2d45dca52de484c6fe3f58d065e7
turalgasimov <turallgasimov@gmail.com>
2026-03-16 06:24:35 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-    print("{} arguments: ".format(len(sys.argv)))
-    for i in range(1, len(sys.argv)): 
-        print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments: ".format(len(sys.argv)))
+        for i in range(1, len(sys.argv)): 
+            print("{}: {}".format(i, sys.argv[i]))
