2c6c680985b87e5645340bbec73f173140d4f218
turalgasimov <turallgasimov@gmail.com>
2026-03-16 06:32:56 +0000

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+import sys
+
 if __name__ == "__main__":
-    if len(argv) == 1:
+    if len(sys.argv) == 1:
         print(0)
     sum = 0
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
     print(sum)
