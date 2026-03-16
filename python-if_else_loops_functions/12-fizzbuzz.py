a1499c3dedf1902058348b62bd4d879d8c9abd93
turalgasimov <turallgasimov@gmail.com>
2026-03-16 03:01:34 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,11 +5,11 @@ def fizzbuzz():
         s = ' '
         if i == 100:
             s = '\n'
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=s)
+        elif i % 3 == 0:
             print("Fizz", end=s)
         elif i % 5 == 0:
             print("Buzz", end=s)
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=s)
         else:
             print(i, end=s)
