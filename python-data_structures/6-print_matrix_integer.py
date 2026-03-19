d3f5bf7fcc7772821e69e81000865e796a479b60
turalgasimov <turallgasimov@gmail.com>
2026-03-19 08:45:26 +0000

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if j == i[len(i) - 1]:
                 print('{:d}'.format(j), end='')
-            print('{:d} '.format(j), end='')
+            else:
+                print('{:d} '.format(j), end='')
         print()     
