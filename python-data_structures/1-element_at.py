a3894079a78c9b7232ace7a43cf02a44ff7f9cd1
turalgasimov <turallgasimov@gmail.com>
2026-03-19 08:14:46 +0000

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if (len(my_list) <= idx) or idx < 0:
-        print('Element at index {:d} is None'.format(idx))
+    if len(my_list) <= idx or idx < 0:
         return None
     else:
-        print('Element at index {:d} is {}'.format(idx, my_list[idx]))
+        return my_list[idx]
