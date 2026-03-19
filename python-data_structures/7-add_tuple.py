a4f97a7bbec7831854ab621e0ab54212e11e0468
turalgasimov <turallgasimov@gmail.com>
2026-03-19 09:54:05 +0000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,7 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         t = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
     else:
         if len(tuple_a) < 2:
+            if len(tuple_a) == 0:
+                t = tuple_b.copy()
+                return t
             t = (tuple_a[0]+tuple_b[0], 0+tuple_b[1])
         elif len(tuple_b) < 2:
+            if len(tuple_b) == 0:
+                t = tuple_a.copy()
+                return t
             t = (tuple_a[0]+tuple_b[0], tuple_a[1]+0)
     return t
