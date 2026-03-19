940128e255503a6ea30a83d33859058744867a33
turalgasimov <turallgasimov@gmail.com>
2026-03-19 11:16:07 +0000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        t = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
-    else:
-        if len(tuple_a) < 2:
-            if len(tuple_a) == 0:
-                t = (0+tuple_b[0], 0+tuple_b[1])
-                return t
-            t = (tuple_a[0]+tuple_b[0], 0+tuple_b[1])
-        if len(tuple_b) < 2:
-            if len(tuple_b) == 0:
-                t = (tuple_a[0]+0, tuple_a[1]+0)
-                return t
-            t = (tuple_a[0]+tuple_b[0], tuple_a[1]+0)
-    return t
+    if 0 < len(tuple_a) < 2:
+        a0 = tuple_a[0]
+        a1 = 0
+    elif len(tuple_a) >= 2:
+        a1 = tuple_a[1]
+    if 0 < len(tuple_b) < 2:
+        b0 = tuple_b[0]
+        b1 = 0
+    elif len(tuple_b) >= 2:
+        b1 = tuple_b[1]
+    return (a0+b0, a1+b1)
