454670c8fac3f2996c255e3944edb1c39885fb41
turalgasimov <turallgasimov@gmail.com>
2026-03-19 13:25:07 +0000

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,10 +4,3 @@ def delete_at(my_list=[], idx=0):
         return my_list
     list = my_list.remove(my_list[idx])
     return my_list
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
