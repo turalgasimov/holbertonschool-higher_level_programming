2be418e92ab25bb0232e201e5fa91fa24493115a
turalgasimov <turallgasimov@gmail.com>
2026-03-23 06:27:10 +0000

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    dict = a_dictionary
-    for i in a_dictionary.values():
-        i *= 2
+    dict = a_dictionary.copy
+    for k, v in dict.items():
+        dict[k] = v*2 
     return dict
