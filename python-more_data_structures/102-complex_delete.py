95bae806ca29c449395b65ca152bc414ce6e4566
turalgasimov <turallgasimov@gmail.com>
2026-03-27 09:25:20 +0000

    102-complex_delete.py

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    
+    for k, v in a_dictionary.items():
+        if v == value:
+            a_dictionary.pop(k)
+    return a_dictionary
