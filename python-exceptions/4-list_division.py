83480c580f27d435efdc69a5ba816f9d99948f25
turalgasimov <turallgasimov@gmail.com>
2026-03-27 23:22:06 +0000

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,3 +15,15 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             res.append(0)
     return res
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
