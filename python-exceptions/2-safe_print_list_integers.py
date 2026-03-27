2ce322bc0776cba21d33cedab7bbcaa73f5700e4
turalgasimov <turallgasimov@gmail.com>
2026-03-27 13:30:35 +0000

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    
+    try:
+        cnt = 0
+        for i in range(x):
+            if type(my_list[i]) is not int:
+                continue
+            print("{:d}".format(my_list[i]), end="")
+            cnt += 1
+        print()
+        return cnt
+    except (TypeError, ValueError):
+        pass
+
+
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
