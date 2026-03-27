3af9a6543713c80af8025b04d4257f3f19a0eb5e
turalgasimov <turallgasimov@gmail.com>
2026-03-27 10:12:00 +0000

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,16 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
         print()
-    except:
-        for i in my_list:
-            print(my_list[i], end='')
+        return x
+    except IndexError:
         print()
+        return i
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
