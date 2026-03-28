8d7e8d81b1dd00fe8bc8455fe07165089090161f
turalgasimov <turallgasimov@gmail.com>
2026-03-28 03:07:01 +0000

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as e:
-        print('Exception: '+str(e), file=sys.stderr)
+    except (TypeError, ValueError) as e:
+        print('Exception: ' + str(e), file=sys.stderr)
         return False
