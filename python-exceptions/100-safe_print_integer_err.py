f84fad5ed1e0009bb60130b5df5ccfcd2c5deeb0
turalgasimov <turallgasimov@gmail.com>
2026-03-28 03:05:12 +0000

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -8,18 +8,3 @@ def safe_print_integer_err(value):
     except Exception as e:
         print('Exception: '+str(e), file=sys.stderr)
         return False
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
