240e14051e2223bbac70b840b867eb80780d4a5d
turalgasimov <turallgasimov@gmail.com>
2026-03-28 03:28:11 +0000

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
