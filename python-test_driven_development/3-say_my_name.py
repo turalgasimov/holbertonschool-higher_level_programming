7b84729ad20764ec5ace261455147f5987b074be
turalgasimov <turallgasimov@gmail.com>
2026-03-29 12:01:22 +0000

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,5 +10,5 @@ def say_my_name(first_name, last_name=""):
 
     if not (isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-    
+
     print(f"My name is {first_name} {last_name}")
