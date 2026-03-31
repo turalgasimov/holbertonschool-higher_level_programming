34361bf42519e5554853352d0b649b2a7da46883
turalgasimov <turallgasimov@gmail.com>
2026-03-31 12:45:36 +0000

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a square."""
+
+
+class Square:
+	"""Defines a square."""
+
+	def __init___():
+		pass
---
+++ python-classes/README.md
@@ -0,0 +1,4 @@
+List of the files and their purposes:
+
+0-square.py -> defines a square.
+
