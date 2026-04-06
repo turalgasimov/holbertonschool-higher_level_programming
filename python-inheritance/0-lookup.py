7bd326e887d68a123e69b3e9fa7623602fd70d4f
turalgasimov <turallgasimov@gmail.com>
2026-04-06 20:58:20 +0000

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''This module returns
+the list of available
+attributes and methods of an object.'''
+
+
+def lookup(obj):
+    '''Returns the list of
+    available attributes and
+    methods of an object'''
+
+    l = []
+    return dir(l)
---
+++ python-inheritance/README.md
@@ -0,0 +1,4 @@
+List of the files and their purposes:
+
+0-lookup.py ->  returns the list of available attributes and methods of an object.
+
