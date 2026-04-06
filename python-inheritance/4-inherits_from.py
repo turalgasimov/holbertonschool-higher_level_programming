d339c7e4732e2fd9a2aa3502ca7ecbba87831be1
turalgasimov <turallgasimov@gmail.com>
2026-04-06 23:00:16 +0000

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     a class that inherited (directly or indirectly) from
     the specified class ; otherwise False.'''
 
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and (type(obj) is not type(a_class))
