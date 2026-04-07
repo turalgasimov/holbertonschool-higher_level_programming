7050491f9b4f4f2f6243fe69c648532120f92559
turalgasimov <turallgasimov@gmail.com>
2026-04-07 03:15:11 +0000

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''This module is child class of BaseGeometry.'''
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''Is child of BaseGeometry.'''
@@ -10,3 +11,18 @@ class Rectangle(BaseGeometry):
             self.__width = width
         if super().integer_validator("height", height):
             self.__height = height
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
