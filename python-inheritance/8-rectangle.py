5f16ef1804e34fac8f7fee835919ebaae7279502
turalgasimov <turallgasimov@gmail.com>
2026-04-07 03:19:56 +0000

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 '''This module is child class of BaseGeometry.'''
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     '''Is child of BaseGeometry.'''
