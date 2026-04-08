acc12ff3417bb973f7cf6731815532bc174dd73d
turalgasimov <turallgasimov@gmail.com>
2026-04-08 19:53:37 +0000

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -47,8 +47,8 @@ def shape_info(self):
     print(f'Area: {self.area()}')
     print(f'Perimeter: {self.perimeter()}')
 
+
 if __name__ == "__main__":
-    # Testing the functionality
     circle = Circle(radius=1)
     rectangle = Rectangle(width=1, height=2)
 
