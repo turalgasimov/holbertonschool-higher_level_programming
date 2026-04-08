385af97b848c3e682980ad9296a81d422c0196f3
turalgasimov <turallgasimov@gmail.com>
2026-04-08 19:09:27 +0000

    task_01_duck_typing.py

--- python-abc/README.md
+++ python-abc/README.md
@@ -2,3 +2,4 @@ List of the files and their purposes:
 
 task_00_abc.py -> ABC class Animal and 2 implementations
 
+task_01_duck_typing.py -> Shape, Rectangle, Circle
---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''This module contains ABC class Shape'''
+from abc import ABC, abstractmethod
+import math as m
+
+
+class Shape(ABC):
+    '''abstract class Shape'''
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(ABC):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return m.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return self.radius * 2 * m.pi
+
+class Rectangle(ABC):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(self):
+    print(f'Area: {self.area()}')
+    print(f'Perimeter: {self.perimeter()}')
