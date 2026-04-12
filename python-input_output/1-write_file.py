bf432077873ec637c0af8ff64acb499e513e0d1b
turalgasimov <turallgasimov@gmail.com>
2026-04-12 22:29:29 +0000

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 '''This module writes to file.'''
 
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """Writes to file."""
     with open(filename, encoding="utf-8") as f:
         write_file = f.write(text)
