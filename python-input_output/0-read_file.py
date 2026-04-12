d9656027847b359ed7ef45d8ed1bdf30e380e830
turalgasimov <turallgasimov@gmail.com>
2026-04-12 21:53:37 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """Reads a file."""
     with open(filename, encoding="utf-8") as f:
-        read_data  = f.read()
-        print(read_data)
+        for line in f:
+            print(line, end='')
