d655d261504222fdde6d0f5a3a45d7cd3fdcc48e
turalgasimov <turallgasimov@gmail.com>
2026-04-13 06:59:27 +0000

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 '''This module  returns the dictionary description.'''
-import json
 
 
 def class_to_json(obj):
     '''returns the dictionary description.'''
-    return json.dumps(obj.__dict__)
+    return vars(obj)
     
