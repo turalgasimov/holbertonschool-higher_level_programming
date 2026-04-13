bf5ed88d5fdce6eabb1a57a397c2ffe37543ec90
turalgasimov <turallgasimov@gmail.com>
2026-04-13 04:26:55 +0000

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -3,8 +3,8 @@
 import json
 
 
-def save_to_json_file(my_obj, filename)::
-    '''writes to text file.'''
+def save_to_json_file(my_obj, filename):
+    '''Writes to text file.'''
     with open(filename, 'w') as f:
         json_data = json.dumps(my_obj)
         for line in json_data:
