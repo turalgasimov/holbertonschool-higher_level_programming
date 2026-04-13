0f2c23b3410cd9a4769fabe52df74c3935f2cdb9
turalgasimov <turallgasimov@gmail.com>
2026-04-13 06:47:13 +0000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-'''This module adds all arguments 
-to a Python list, and then save them to a file.'''
-import json, sys
-from 5-save_to_json_file import save_to_json_file as s
-from 6-load_from_json_file import load_from_json_file as l
+'''This module adds all arguments
+to a Python list, and then them to a file.'''
+import json
+import sys
+import os
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_n_save():
-    '''adds all arguments 
+    '''adds all arguments
     to a Python list, and then save them to a file.'''
 
     filename = "add_item.json"
+    new_list = []
 
-    with open(filename, "w") as f:
-        data = l(filename)
-        data = list(data)
+    for i in range(1, len(sys.argv)):
+        new_list.append(sys.argv[i])
 
-        for _ in sys.argv:
-            data.append(_)
+    try:
+        data = load(filename)
+    except FileNotFoundError:
+        data = []
 
-        s(data, filename)
+    data += new_list
+    save(data, filename)
+
+
+if __name__ == "__main__":
+    add_n_save()
