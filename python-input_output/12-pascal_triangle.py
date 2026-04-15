46d821df560c7185d28c6f1b98a0a80ef1562d6f
turalgasimov <turallgasimov@gmail.com>
2026-04-15 17:06:11 +0000

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,3 +10,18 @@ def pascal_triangle(n):
 
     if n <= 0:
         return pascal_list
+
+    for i in range(1, n + 1):
+        row = [1] * i
+        pascal_list.append(row)
+
+        if i-1 > 1:
+            for j in range(1, len(row)-1):
+                row[j] = pascal_list[i-2][j-1] + pascal_list[i-2][j]
+                pascal_list[i-1] = row
+  
+    return pascal_list
+
+triangle = pascal_triangle(5)
+for row in triangle:
+    print(row)
