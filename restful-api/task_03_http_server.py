93a3541d2fe7c3b6b3a0d6ef36cfc9a9b7b7b62b
turalgasimov <turallgasimov@gmail.com>
2026-04-21 08:21:43 +0000

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,12 +7,24 @@ class RequestHandler(BaseHTTPRequestHandler):
     """class RequestHandler"""
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write("Hello, this is a simple API!\n".encode("utf-8"))
-
-
-server = HTTPServer(("localhost", 8000), RequestHandler)
-print("Server started at http://localhost:8000")
-server.serve_forever()
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode("utf-8"))
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            dataset ={"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(f"{dataset}".encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write("OK".encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write("404 Not Found".encode("utf-8"))
