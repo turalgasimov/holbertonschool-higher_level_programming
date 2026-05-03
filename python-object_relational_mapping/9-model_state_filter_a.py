28b19dbbfbf02b7045f2b112d3a56a9d7a4c02da
turalgasimov <turallgasimov@gmail.com>
2026-05-03 19:46:29 +0000

    9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,17 +12,18 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            username, password, database
-        )
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, database)
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(
+    query = session.query(State).filter(
         State.name.like("%a%")
-        ).order_by(State.id):
-            print("{}: {}".format(state.id, state.name))
+    ).order_by(State.id)
+
+    for state in query:
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
