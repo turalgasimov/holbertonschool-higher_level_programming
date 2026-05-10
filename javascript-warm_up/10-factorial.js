ee10cb153ebafea2cf9d5f8c5f043295c9c6fd86
turalgasimov <turallgasimov@gmail.com>
2026-05-10 23:19:13 +0400

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
-function factorial (n) {
+function factorial(n) {
   if (n === 0 || isNaN(n)) {
-  return 1;
+    return 1;
   } else {
-  return n * factorial(n - 1);
+    return n * factorial(n - 1);
   }
 }
 console.log(factorial(myVar));
